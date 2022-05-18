a8b2c0846c34a7b3735d7425d373077ec46d9adc
shahriarlimon <shahriarwebdev@gmail.com>
2022-05-18 21:59:51 +0600

    initialised and implemented post api

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.bugcd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+/* 
+client.connect(err => {
+   
+    // perform actions on the collection object
+    console.log('connected');
+  }); */
+  async function run() {
+      try{
+        await client.connect();
+        const toDoLists = client.db("to_do_app").collection("to_do_lists");
+        app.post('/do_lists', async(req,res)=>{
+            const list = req.body;
+            const result = await toDoLists.insertOne(list);
+            res.send(result);
+        })
+      }finally{
+
+      }
+  }
+  run().catch(console.dir);
+
+/* http://localhost:4000/ */
+app.get("/", async (req, res) => {
+  res.send("to do app is running");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
